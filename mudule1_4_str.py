870f7170304e809cb088fe87e76b0c3a39789292
Dima <72711928+dmytros977@users.noreply.github.com>
2024-11-09 14:53:03 +0300

    Add files via upload

---
+++ mudule1_4_str.py
@@ -0,0 +1,12 @@
+print("Здравствуйте!")
+print("Пожалуйста, укажите ваше имя: ")
+your_name = input()
+my_string = input('Какой курс Вы выбрали?: ')
+print("Поздравляем! Желаем успеха в Вашем обучении!", my_string,',' "Дмитрий.")
+print(len(my_string))
+print('Разработчик Python' .upper())
+print('Разработчик Python' .lower())
+print('Р аз раб  отчик:Pyth on .'.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
